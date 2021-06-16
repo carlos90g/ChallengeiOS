94f4e28f28565efa6a5b61f01aaf28cd02f463c9
Carlos Garcia <carlos.garcia@fluxit.com.ar>
2021-06-16 11:13:57 -0300

    Removed storyboard

--- ChallengeiOS/ChallengeiOS/HomeViewController.swift
+++ ChallengeiOS/ChallengeiOS/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  ChallengeiOS
 //
 //  Created by Carlos Garcia on 16/06/2021.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
